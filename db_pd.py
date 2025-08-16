10b8c028b2d27c65b57382fa1e7432e9531728a5
JesusCD <jesusdanielgraterols@gmail.com>
2025-08-15 22:33:45 -0400

    pequeña modificacion de la creaccion de carpetas

--- db_pd.py
+++ db_pd.py
@@ -2,6 +2,7 @@ import pandas as pd
 import mysql.connector
 from mysql.connector import Error
 
+
 def obtener_ventas_por_fecha():
     try:
         connection = mysql.connector.connect(
@@ -11,12 +12,10 @@ def obtener_ventas_por_fecha():
             password="",
             database="proyecto_informe"
         )
-        fecha_inicio="2025-06-28"
-        fecha_fin="2025-07-01"
-
-        
+        global f_1, f_2 
+        fecha_inicio , fecha_fin = ingresar_fecha()
+        f_1, f_2 = fecha_inicio, fecha_fin
         query = "SELECT id,nombre,tipo,precio,fecha FROM ventas Where fecha >= %s and fecha<= %s"  
-        
         
         dataframe = pd.read_sql(query, connection,params=(fecha_inicio, fecha_fin))
         
@@ -32,8 +31,25 @@ def obtener_ventas_por_fecha():
             connection.close()
             print("Conexión a la base de datos cerrada.")
 
+def ingresar_fecha():
+     v=True
+     while v==True:
+        
+      print("Ingrese la fecha en formato YYYY-MM-DD")
+      primera_fecha = input("Fecha de inicio: ")
+      segunda_fecha = input("Fecha de fin: ")
+    
+      if primera_fecha > segunda_fecha:
+          print("La fecha de inicio no puede ser posterior a la fecha de fin.")
+          v=False
+        
+      return primera_fecha, segunda_fecha
+        
+    
+  
+
 if __name__ == "__main__":
     df = obtener_ventas_por_fecha()
     if df is not None:
-        df.to_excel("ventas_por_fecha3.xlsx", index=False)
+        df.to_excel(f"ventas_del_{f_1}_al_{f_2}.xlsx", index=False)
         print("Datos exportados a ventas_por_fecha.xlsx")
